63d6b71b5bb8a1236f346336a84284aeb26eea14
brydenfogelman <bryden1995@gmail.com>
2015-03-29 23:17:50 -0700

    Adding in soundmanager
    Trying to get the url we are pulling from sound cloud to play

--- public/javascript/the-sound-of-weather.js
+++ public/javascript/the-sound-of-weather.js
@@ -1,16 +1,27 @@
 $(document).on('ready', function(){
-    
+
     // init up soundcloud
     SC.initialize({
         client_id: '63a8bfefff63b0f9386d4a03184c3b94'
     });
-    
+
+    //sound manager setup
+    soundManager.setup({
+        url: '/path/to/swf-files/',
+        flashVersion: 9, // optional: shiny features (default = 8)
+        // optional: ignore Flash where possible, use 100% HTML5 mode
+        // preferFlash: false,
+        onready: function() {
+            // Ready to use; soundManager.createSound() etc. can now be called.
+        }
+    });
+
     // set url for wunderground
     var wu_url = "http://api.wunderground.com/api/bebaf6d06e2bf9b4/geolookup/conditions/q/"; // state/city.json OR city.json
     var sc_url_header = "http://api.soundcloud.com/tracks";
     //var sc_url_client_id = "
     //file://api.soundcloud.com/tracks?q=buskers&license=cc-by-sa&client_id=63a8bfefff63b0f9386d4a03184c3b94&format=json&_status_code_map[302]=200
-    
+
     $("#btn-load").click(function() {
         var city = $("#query-field").val() + '.json';
         //console.log(city);
@@ -52,8 +63,11 @@ $(document).on('ready', function(){
                     success: function(response){
                         // need an if statement to deal with the case where a city searc
                         // has 2 results
-                        //var weather = response.current_observation.weather;
                         console.log(response);
+                        console.log(response[0].stream_url);
+                        SC.stream(response[0].stream_url, function(sound){
+                            sound.play();
+                        });
                     },
                     error: function(){
                         console.log("error");
@@ -64,8 +78,8 @@ $(document).on('ready', function(){
                 console.log("error");
             }
         });
-        
-        
+
+
         /*
         $.ajax({
             url: sc_url_header,
@@ -88,9 +102,9 @@ $(document).on('ready', function(){
         });
         */
     });
-    
-    
-    
+
+
+
     // find all sounds of buskers licensed under 'creative commons share alike'
     /*
     // find all sounds of buskers licensed under 'creative commons share alike'
